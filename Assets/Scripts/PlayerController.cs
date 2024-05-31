9772cee852bc1eb135ef48101666e0dc4d147df1
Tom St.Jean <tpstjn@gmail.com>
2024-05-30 20:15:45 -0600

    Dash working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,25 +11,65 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float MOVE_SPEED;
     [SerializeField] private float DASH_SPEED;
     [SerializeField] private float DASH_TIME;
+    [SerializeField] private float DASH_COOLDOWN;
 
     private Rigidbody2D _rbody;
-    // private BoxCollider2D _collider;
+    private BoxCollider2D _collider;
+
+    private bool _isDashing;
+    private bool _dashStopped;
+    private double _dashCooldown;
+    private double _timeSinceDash;
 
     // Start is called before the first frame update
     void Start()
     {
         _rbody = GetComponent<Rigidbody2D>();
-        // _collider = GetComponent<BoxCollider2D>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            HandleDash();
+        }
+        else
+        {
+            HandleCooldowns();
+        }
+    }
 
+    void HandleDash()
+    {
+        if (_dashCooldown <= 0.0 && _rbody.velocity.magnitude >= 0.1f)
+        {
+            Debug.Log("Player is dashing");
+            _isDashing = true;
+            _dashStopped = false;
+            _dashCooldown = DASH_COOLDOWN;
+            _timeSinceDash = 0.0;
+            _rbody.velocity *= DASH_SPEED;
+        }
+
+
+    }
+
+    void HandleCooldowns()
+    {
+        if (_dashCooldown > 0.0) _dashCooldown -= Time.deltaTime;
+        if (_timeSinceDash < DASH_TIME) _timeSinceDash += Time.deltaTime;
+        if (_timeSinceDash >= DASH_TIME && !_dashStopped)
+        {
+            _dashStopped = true;
+            _isDashing = false;
+            _rbody.velocity = Vector2.zero;
+        }
     }
 
     private void FixedUpdate()
     {
-        _rbody.velocity = new Vector2(MOVE_SPEED * Input.GetAxis("Horizontal"), MOVE_SPEED * Input.GetAxis("Vertical"));
+        if (!_isDashing) _rbody.velocity = new Vector2(MOVE_SPEED * Input.GetAxis("Horizontal"), MOVE_SPEED * Input.GetAxis("Vertical"));
     }
 }
